fd2d3abff8b0892ede8802459f60a3f08df373f9
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-12-17 12:45:16 +0700

    Add files via upload

---
+++ LatihanMVC/src/edu/rizkiadam/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkiadam.latihanmvc.event;
+
+import edu.rizkiadam.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author 
+ * Nama  : Santi Susanti
+ * Nim   : 10117098
+ * Kelas : IF-3
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+}
---
+++ LatihanMVC/src/edu/rizkiadam/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkiadam.latihanmvc.model;
+
+/**
+ *
+ * @author 
+ * Nama  : Santi Susanti
+ * Nim   : 10117098
+ * Kelas : IF-3
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
